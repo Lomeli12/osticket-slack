f13663d7faa2c5c5defbee50185f4f6c4110aea1
Lomeli12 <anthonylomeli11@gmail.com>
2016-07-06 10:19:29 -0700

    Added myself to author list

--- slack/plugin.php
+++ slack/plugin.php
@@ -4,7 +4,7 @@ return array(
     'id' =>             'osticket:slack', 
     'version' =>        '0.2',
     'name' =>           'Slack notifier',
-    'author' =>         'Johannes mueller-werner/Thammanna Jammada',
+    'author' =>         'Anthony Lomeli/Johannes mueller-werner/Thammanna Jammada',
     'description' =>    'Notify Slack on new ticket.',
     'url' =>            'https://github.com/thammanna/osticket-slack',
     'plugin' =>         'slack.php:SlackPlugin',        
